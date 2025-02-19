05ed3f5dbc30e2bef73eaca45c3185c906598969
nehalskdev <nikesk1999@gmail.com>
2025-02-19 11:35:28 +0530

    splitted search component

--- src/App.jsx
+++ src/App.jsx
@@ -60,25 +60,30 @@ export default function App() {
 }
 
 function NavBar() {
-  const [query, setQuery] = useState("");
-
   return (
     <nav className="nav-bar">
       <div className="logo">
         <span role="img">🍿</span>
         <h1>usePopcorn</h1>
       </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+      <Search />
       <p className="num-results">
         Found <strong>x</strong> results
       </p>
     </nav>
+  );
+}
+
+function Search() {
+  const [query, setQuery] = useState("");
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
   );
 }
 
