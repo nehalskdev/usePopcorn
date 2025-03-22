955fd42f11ad020ceef9039f1e9f7df3bdcc2726
nehalskdev <nikesk1999@gmail.com>
2025-03-22 11:58:33 +0530

    made the function async inside useEffect

--- src/App-v1.jsx
+++ src/App-v1.jsx
@@ -55,11 +55,17 @@ const KEY = "6bc0145b";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const query = "Kick";
 
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=kick`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
   }, []);
 
   return (
